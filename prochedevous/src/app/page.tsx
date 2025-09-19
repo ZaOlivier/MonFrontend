dc9bab5ac29de69d6dde3cfb82dd9dd256b96e9e
zaouli franck <zaoulifranck0@gmail.com>
2025-09-19 08:47:49 +0000

    recommence la securisation

--- prochedevous/src/app/page.tsx
+++ prochedevous/src/app/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
-   <img src="@/app/favicon.ico" alt="carre" 
+   <Image src="@/app/favicon.ico" alt="carre" 
    width={10} height={10}
    />
-   on reommence
+   on reommence encoe
    
    </>
   );
