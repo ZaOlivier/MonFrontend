2b10085547e2b3b6a4f10cbb739a327cc1cd6d4c
zaouli franck <zaoulifranck0@gmail.com>
2025-10-04 18:01:10 +0000

    er

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Card3pro from "@/components/article/card3pro";
-// import CardHorizontal6Prod from "@/components/article/cardAcceuil";
+import CardHorizontal6Prod from "@/components/article/cardAcceuil";
 import SimulationRecherche from "@/components/Navigation/simulationBarreRech";
 
 
@@ -13,7 +13,7 @@ export default function Home () {
      <div><h5 className="bg-white text-blue-200 text-center border-1 w-100%">Expose tes gammes de produit sur Olivi</h5></div>
       <SimulationRecherche/>
       <Card3pro/>
-      {/* <CardHorizontal6Prod/> */}
+      <CardHorizontal6Prod/>
       <Card3pro/>
       <Card3pro/>
    </>
