62c3664ab0a910bc0692de4011ed7aed523ee87b
zaouli franck <zaoulifranck0@gmail.com>
2025-10-04 16:56:38 +0000

    ch3

--- frontend/src/app/produit/[id]/page.tsx
+++ frontend/src/app/produit/[id]/page.tsx
@@ -1,38 +1,38 @@
-// import {Props } from "@/types/types";
-import axios from "axios";
-import Image from "next/image";
+// // import {Props } from "@/types/types";
+// import axios from "axios";
+// import Image from "next/image";
 
-const fetchProduit = async (id:string) => {
-  try {
-    const res = await axios.get(`https://olivi.onrender.com/produit/article/${id}`);
-    return res.data;
-  } catch (err) {
-    console.error("Erreur lors de la récupération du produit :", err);
-    return null;
-  }
+// const fetchProduit = async (id:string) => {
+//   try {
+//     const res = await axios.get(`https://olivi.onrender.com/produit/article/${id}`);
+//     return res.data;
+//   } catch (err) {
+//     console.error("Erreur lors de la récupération du produit :", err);
+//     return null;
+//   }
   
-};
+// };
 
-// { params: Paramettre })
+// // { params: Paramettre })
 
-export default async function Page({ params }:{params : {id:string}})  {
-  const produit = await fetchProduit(params.id)
-  console.log(produit);
+// export default async function Page({ params }:{params : {id:string}})  {
+//   const produit = await fetchProduit(params.id)
+//   console.log(produit);
  
-  // encore
+//   // encore
   
-  if (!produit) {
-    return <p>Produit introuvable</p>;
-  }
+//   if (!produit) {
+//     return <p>Produit introuvable</p>;
+//   }
 
-  return (
-    <div>
-      <Image src={produit.image} alt={produit.nom} width={230} height={300}/>
-      <h1>{produit.nom}</h1>
-      <p>{produit.prix} </p>
-      <p>{produit.description}</p>
-      {/* Ajoute ici le reste des infos */}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <Image src={produit.image} alt={produit.nom} width={230} height={300}/>
+//       <h1>{produit.nom}</h1>
+//       <p>{produit.prix} </p>
+//       <p>{produit.description}</p>
+//       {/* Ajoute ici le reste des infos */}
+//     </div>
+//   );
+// }
 
