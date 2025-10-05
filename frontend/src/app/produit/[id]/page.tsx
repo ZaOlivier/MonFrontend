964f3e0d8a51bb88325dff43eff8f788105c8f99
zaouli franck <zaoulifranck0@gmail.com>
2025-10-05 20:30:38 +0000

    hejd

--- frontend/src/app/produit/[id]/page.tsx
+++ frontend/src/app/produit/[id]/page.tsx
@@ -1,42 +1,42 @@
-// import {Props } from "@/types/types";
-import AddToCartButton from "@/components/button/btnAjouterPanier";
-import SimulationRecherche from "@/components/Navigation/simulationBarreRech";
-import axios from "axios";
-import Image from "next/image";
+// // import {Props } from "@/types/types";
+// import AddToCartButton from "@/components/button/btnAjouterPanier";
+// import SimulationRecherche from "@/components/Navigation/simulationBarreRech";
+// import axios from "axios";
+// import Image from "next/image";
 
-const fetchProduit = async (id:string) => {
-  try {
-    const res = await axios.get(`https://olivi.onrender.com/produit/article/${id}`);
-    return res.data;
-  } catch (err) {
-    console.error("Erreur lors de la récupération du produit :", err);
-    return null;
-  }
+// const fetchProduit = async (id:string) => {
+//   try {
+//     const res = await axios.get(`https://olivi.onrender.com/produit/article/${id}`);
+//     return res.data;
+//   } catch (err) {
+//     console.error("Erreur lors de la récupération du produit :", err);
+//     return null;
+//   }
   
-};
+// };
 
 
-export default async function Page({ params }:{params : {id:any}})  {
-  const produit = await fetchProduit(params.id)
-  console.log(produit);
+// export default async function Page({ params }:{params : {id:any}})  {
+//   const produit = await fetchProduit(params.id)
+//   console.log(produit);
  
-  // encore
+//   // encore
   
-  if (!produit) {
-    return <p>Produit introuvable</p>;
-  }
+//   if (!produit) {
+//     return <p>Produit introuvable</p>;
+//   }
 
-  return (
-    <>
-    <SimulationRecherche/>
-    <div>
-      <Image src={produit.image} alt={produit.nom} width={230} height={300}/>
-      <h1>{produit.nom}</h1>
-      <p>{produit.prix} </p>
-      <p>{produit.description}</p>
-      <AddToCartButton produit={produit}/>
-    </div>
-    </>
-  );
-}
-// b
+//   return (
+//     <>
+//     <SimulationRecherche/>
+//     <div>
+//       <Image src={produit.image} alt={produit.nom} width={230} height={300}/>
+//       <h1>{produit.nom}</h1>
+//       <p>{produit.prix} </p>
+//       <p>{produit.description}</p>
+//       <AddToCartButton produit={produit}/>
+//     </div>
+//     </>
+//   );
+// }
+// // b
